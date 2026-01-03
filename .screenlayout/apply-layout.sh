c12e69d2a13abb371a94a1fd5aea05490a4b6a6c
ChiefGyk3D <19499446+ChiefGyk3D@users.noreply.github.com>
2026-01-03 12:35:11 -0500

    fix hdmi detection

--- .screenlayout/apply-layout.sh
+++ .screenlayout/apply-layout.sh
@@ -4,7 +4,8 @@
 export DISPLAY=:1
 export XAUTHORITY="$HOME/.Xauthority"
 
-if nvidia-settings -q dpys | grep -q "HDMI-0) (connected, enabled)"; then
+# Check if HDMI-0 is connected (with or without enabled)
+if nvidia-settings -q dpys | grep -q "HDMI-0) (connected"; then
     ~/.screenlayout/nvidia-capture.sh
 else
     ~/.screenlayout/nvidia-base.sh
